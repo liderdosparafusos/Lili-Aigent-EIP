a1ddc7ef96ff90291dbf52f1497953e03552c9b1
liderdosparafusos <liderdosparafusos@gmail.com>
2025-12-23 18:14:22 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
+console.log('ROOT ELEMENT:', rootElement);
+
+const root = ReactDOM.createRoot(
+  rootElement || document.body
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{ color: 'white', padding: 20 }}>
+      React carregou 🚀
+    </div>
   </React.StrictMode>
 );
